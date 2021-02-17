a2fda5d929c3552ebf98007e27044f0066352fdd
Sebastian Hulse <seb.hulse@yahoo.co.uk>
2021-02-17 19:43:42 +0000

    final separate portait and landscape photo inputs

--- render_all.sh
+++ render_all.sh
@@ -6,17 +6,16 @@ RESET='\033[0m'
 
 # ALL FILE NAMES MUST HAVE NO SPACES IN AND MUST BE UNIQUELY NAMED
 
-cd media/in/photo_in/portrait
-
 echo
 echo -e "${CYAN}*** Starting Portrait Photo Renders ***${RESET}"
 echo
 
-# loop through all .jpg images in media/in/photo_in
-for file in *.jpg;
+# loop through all .jpg images in media/in/photo_in/portrait
+for file in media/in/photo_in/portrait/*.jpg;
 do
-  # trim file name
-  name=${file%.jpg}
+  # extract base name from path and trim
+  basename=$(basename $file)
+  name=${basename%.jpg}
 
   # create output file names
   photo_200x200=""$name"_portrait_200x200.jpg"
@@ -24,34 +23,32 @@ do
   photo_640x640=""$name"_portrait_640x640.jpg"
   photo_1280xAR=""$name"_portrait_1280xAR.jpg"
 
-  # generate photo_200x200 with width and height 200px nd cropping if aspect ratio differs
-  ffmpeg -i "$file" -vf "crop='min(iw,1*ih)':'min(iw/1,ih)',scale=200:200" "../../../out/photo_out/$photo_200x200" -n
-  # generate photo_480x720 with width 480px and height 720px and cropping if aspect ratio differs
-  ffmpeg -i "$file" -vf "crop='min(iw,1.5*ih)',scale=480:720" "../../../out/photo_out/$photo_480x720" -n
-  # generate photo_640x640 with width and height 640px and cropping if aspect ratio differs
-  ffmpeg -i "$file" -vf "crop='min(iw,1*ih)':'min(iw/1,ih)',scale=640:640" "../../../out/photo_out/$photo_640x640" -n
-  # generate photo_1280xAR with width 1280px and by maintaining the original aspect ratio
-  ffmpeg -i "$file" -vf "scale=1280:-2" "../../out/photo_out/$photo_1280xAR" -n
+  # generate photo_200x200 with width and height 200px
+  ffmpeg -i "$file" -vf "crop='min(iw,1*ih)':'min(iw/1,ih)',scale=200:200" "media/out/photo_out/$photo_200x200" -n
+  # generate photo_480x720 with width 480px and height 720px
+  ffmpeg -i "$file" -vf "crop='min(iw,ih/1.5)':'min(1.5*iw,ih)',scale=480:720" "media/out/photo_out/$photo_480x720" -n
+  # generate photo_640x640 with width and height 640px
+  ffmpeg -i "$file" -vf "crop='min(iw,1*ih)':'min(iw/1,ih)',scale=640:640" "media/out/photo_out/$photo_640x640" -n
+  # generate photo_1280xAR with width 1280px
+  ffmpeg -i "$file" -vf "scale=1280:-2" "media/out/photo_out/$photo_1280xAR" -n
 
 
   # status log
   echo
-  echo "Photo optimised: $file"
+  echo "Photo optimised: $basename"
 
 done
-
-
-cd ../landscape
 
 echo
 echo -e "${CYAN}*** Starting Landscape Photo Renders ***${RESET}"
 echo
 
-# loop through all .jpg images in media/in/photo_in
-for file in *.jpg;
+# loop through all .jpg images in media/in/photo_in/landscape
+for file in media/in/photo_in/landscape/*.jpg;
 do
-  # trim file name
-  name=${file%.jpg}
+  # extract base name from path and trim
+  basename=$(basename $file)
+  name=${basename%.jpg}
 
   # create output file names
   photo_200x200=""$name"_landscape_200x200.jpg"
@@ -59,34 +56,32 @@ do
   photo_640x640=""$name"_landscape_640x640.jpg"
   photo_1920xAR=""$name"_landscape_1920xAR.jpg"
 
-  # generate photo_200x200 with width and height 200px nd cropping if aspect ratio differs
-  ffmpeg -i "$file" -vf "crop='min(iw,1*ih)':'min(iw/1,ih)',scale=200:200" "../../../out/photo_out/$photo_200x200" -n
-  # generate photo_720x480 with width 720px and height 480px and cropping if aspect ratio differs
-  ffmpeg -i "$file" -vf "crop='min(iw,1.5*ih)':'min(iw/1.5,ih)',scale=720:480" "../../../out/photo_out/$photo_720x480" -n
-  # generate photo_640x640 with width and height 640px and cropping if aspect ratio differs
-  ffmpeg -i "$file" -vf "crop='min(iw,1*ih)':'min(iw/1,ih)',scale=640:640" "../../../out/photo_out/$photo_640x640" -n
-  # generate photo_1920x1280 with width 1920px and height 1280px and cropping if aspect ratio differs
-  ffmpeg -i "$file" -lavfi "scale=1920-2" "../../out/photo_out/$photo_1920xAR" -n
+  # generate photo_200x200 with width and height 200px
+  ffmpeg -i "$file" -vf "crop='min(iw,1*ih)':'min(iw/1,ih)',scale=200:200" "media/out/photo_out/$photo_200x200" -n
+  # generate photo_720x480 with width 720px and height 480px
+  ffmpeg -i "$file" -vf "crop='min(iw,1.5*ih)':'min(iw/1.5,ih)',scale=720:480" "media/out/photo_out/$photo_720x480" -n
+  # generate photo_640x640 with width and height 640px
+  ffmpeg -i "$file" -vf "crop='min(iw,1*ih)':'min(iw/1,ih)',scale=640:640" "media/out/photo_out/$photo_640x640" -n
+  # generate photo_1920x1280 with width 1920px and by maintaining the original aspect ratio
+  ffmpeg -i "$file" -vf "scale=1920:-2" "media/out/photo_out/$photo_1920xAR" -n
 
   # status log
   echo
-  echo "Photo optimised: $file"
+  echo "Photo optimised: $basename"
 
 done
-
-cd ../../video_in
-
 
 echo
 echo -e "${CYAN}*** Starting Video Renders ***${RESET}"
 echo
 
 # loop through all .mov videoa in media/in/video_in
-for file in *.mov;
+for file in media/in/video_in/*.mov;
 do
 
-  # trim file name
-  name=${file%.mov}
+  # extract base name from path and trim
+  basename=$(basename $file)
+  name=${basename%.mov}
 
   # create file name
   video_1920xAR=""$name"_video_1920xAR.mov"
@@ -116,21 +111,19 @@ do
   # - fade in and out by set $fade_duration (fade=d=$fade_duration,fade=t=out:st=$fade_out:d=$fade_duration)
   # - speed video up by the calculated $factor to be close to the $target_duration (setpts=PTS/$factor)
   # - remove audio (-an)
-  ffmpeg -i "$file" -i ../../../watermark.png -lavfi "scale=1920:-2,overlay=x=(1920-340):y=(1080-100),format=yuv420p,fade=d=$fade_duration,fade=t=out:st=$fade_out:d=$fade_duration,setpts=PTS/$factor" -an "../../out/video_out/$video_1920xAR" -n
+  ffmpeg -i "$file" -i "watermark.png" -lavfi "scale=1920:-2,overlay=x=(1920-340):y=(1080-100),format=yuv420p,fade=d=$fade_duration,fade=t=out:st=$fade_out:d=$fade_duration,setpts=PTS/$factor" -an "media/out/video_out/$video_1920xAR" -n
 
   # calculate duration of video after alteration
-  duration_after=$(ffprobe -i "../../out/video_out/$video_1920xAR" -show_entries format=duration -v quiet -of csv="p=0")
+  duration_after=$(ffprobe -i "media/out/video_out/$video_1920xAR" -show_entries format=duration -v quiet -of csv="p=0")
 
   # status log
   echo
-  echo "Video optimised: $file"
+  echo "Video optimised: $basename"
   echo "Duration before: $duration_before"
   echo "Duration after: $duration_after"
   echo
 
 done
-
-cd ../../..
 
 echo
 echo -e "${PURPLE}*** Completed All Renders ***${RESET}"
